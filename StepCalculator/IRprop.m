a7ed94b38705be655e4e8b00c2246c6553840a73
DrDanRyan <danryanfl@yahoo.com>
2013-09-13 15:08:02 -0400

    Implement IRProp; fix Rprop

--- StepCalculator/IRprop.m
+++ StepCalculator/IRprop.m
@@ -1,6 +1,6 @@
-classdef Rprop < StepCalculator
-   % Basic resilient backprop (Rprop). Should only be used in full batch
-   % mode (for mini-batches use RMSprop).
+classdef IRprop < StepCalculator
+   % "Improved Rprop" with weight backtracking. 
+   % Should only be used in full batch mode (for mini-batches use RMSprop).
    
    properties
       upFactor
@@ -10,11 +10,13 @@ classdef Rprop < StepCalculator
       initialRate
       
       rates
+      prevStep
+      prevLoss
       prevGrad
    end
    
    methods
-      function obj = Rprop(initialRate, varargin)
+      function obj = IRprop(initialRate, varargin)
          p = inputParser();
          p.addParamValue('upFactor', 1.2);
          p.addParamValue('downFactor', .5);
@@ -31,37 +33,47 @@ classdef Rprop < StepCalculator
       
       
       function take_step(obj, x, t, model, ~)
-         grad = model.gradient(x, t);
-         obj.update_rates(grad, model);
-         steps = cellfun(@(rate, grad) rate.*sign(grad), obj.rates, grad, ...
-                          'UniformOutput', false);
-         model.increment_params(steps);
-         obj.prevGrad = grad;
-      end
-      
-      function update_rates(obj, grad, model)
+         [grad, y] = model.gradient(x, t);
+         loss = model.compute_loss(y, t);
+         step = cell(size(grad));
+         
          if isempty(obj.prevGrad)
             obj.rates = cell(size(grad));
             for i = 1:length(grad)
                obj.rates{i} = obj.initialRate*model.gpuState.ones(size(grad{i}));
+               step{i} = obj.rates{i}.*sign(grad{i});
             end
          else
+            isLossIncrease = loss > obj.prevLoss;
             for i = 1:length(grad)
                gradProduct = grad{i}.*obj.prevGrad{i};
+               
                upIdx = gradProduct > 0;
                downIdx = gradProduct < 0;
-
+               
                obj.rates{i}(upIdx) = min(obj.maxRate, ...
                                          obj.upFactor*obj.rates{i}(upIdx));
                obj.rates{i}(downIdx) = max(obj.minRate, ...
                                            obj.downFactor*obj.rates{i}(downIdx));
+               
+               grad{i}(downIdx) = 0;                         
+               step{i} = obj.rates{i}.*sign(grad{i});
+               if isLossIncrease
+                  step{i}(downIdx) = -obj.prevStep{i}(downIdx);
+               end
             end
          end
+         model.increment_params(step);
+         obj.prevStep = step;
+         obj.prevLoss = loss;
+         obj.prevGrad = grad;
       end
       
       function reset(obj)
          obj.rates = [];
          obj.prevGrad = [];
+         obj.prevLoss = [];
+         obj.prevStep = [];
       end
    end
 end
