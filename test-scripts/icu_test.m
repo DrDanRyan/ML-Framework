daf3b561d6efda303bfb78a3d5a73f4694147a25
DrDanRyan <danryanfl@yahoo.com>
2013-09-13 16:37:43 -0400

    Modify icu_test to test a single net

--- test-scripts/icu_test.m
+++ test-scripts/icu_test.m
@@ -1,26 +1,22 @@
 clear all
+close all
 load Citi_data
 
 %% parameters
-N = 10;
-nDims = 90;
-Layer1 = 20;
-Layer2 = 20;
-nnets = cell(1, N);
-preprocessors = cell(1, N);
+Layer1 = 100;
+Layer2 = 300;
 
 nFolds = 5;
-sampleSize = 2400;
 
-learnRate = .01;
+learnRate = .05;
 minRate = 1e-7;
 maxRate = 50;
-upFactor = 1.2;
+upFactor = 1.3;
 downFactor = .5;
-nEpochs = 60;
+nEpochs = 100;
 
-maxoutLayers = 5;
-inputDropout = .1;
+maxoutLayers = 8;
+inputDropout = .4;
 
 %momentum = .85;
 %lrDecay = .998; 
@@ -37,23 +33,19 @@ trainer.reporter = ConsoleReporter();
 trainer.trainingSchedule = BasicMomentumSchedule(learnRate, 0, nEpochs);
 
 %% Initialize nnets and preprocessors
-for i = 1:N
-   nnets{i} = FeedForwardNet('inputDropout', inputDropout);
-   nnets{i}.hiddenLayers = {MaxoutHiddenLayer(nDims, Layer1, maxoutLayers), ...
-                               MaxoutHiddenLayer(Layer1, Layer2, maxoutLayers, ...
-                                                   'initType', 'sparse', ...
-                                                   'initScale', 15)};
-   nnets{i}.outputLayer = SVMOutputLayer(Layer2, 'hingeExp', 2, 'L2Penalty', .001);
+nnet = FeedForwardNet('inputDropout', inputDropout);
+nnet.hiddenLayers = {MaxoutHiddenLayer(187, Layer1, maxoutLayers)};%, ...
+%                      TanhHiddenLayer(Layer1, Layer2, ...
+%                                                 'initType', 'sparse', ...
+%                                                 'initScale', 15)};
 
-   preprocessors{i} = RandomProjection(187, nDims);
-   %preprocessors{i} = Identity();
-end
+nnet.outputLayer = SVMOutputLayer(Layer1, 'hingeExp', 2); %, 'L1Penalty', 1e-4, 'L2Penalty', 1e-5);
 targets(targets == 0) = -1;
 
 %% Train base learners on CV partition
 hold_outs = cross_validate_partition(4000, nFolds);
 gpuState = GPUState();
-outputs = gpuState.zeros(N, 4000);
+outputs = gpuState.zeros(1, 4000);
 
 for foldIdx = 1:nFolds
    
@@ -65,73 +57,19 @@ for foldIdx = 1:nFolds
    trainTargets = single(gpuArray(targets(:, trainIdx)));
    validInputs = single(gpuArray(data(:, validIdx)));
    validTargets = single(gpuArray(targets(:, validIdx)));
-   
-   for netIdx = 1:N
-      sampleIdx = randsample(length(trainIdx), sampleSize)';
-      tInputs = preprocessors{netIdx}.transform(trainInputs(:, sampleIdx));
-      tTargets = trainTargets(:, sampleIdx);
-      vInputs = preprocessors{netIdx}.transform(validInputs); 
-      trainer.dataManager = FullBatch(tInputs, tTargets, vInputs, validTargets);
-      trainer.model = nnets{netIdx};
-      trainer.reset();
-      trainer.train();
-      outputs(netIdx, validIdx) = nnets{netIdx}.output(vInputs);
-      fprintf('\n');
-   end
-   
+   trainer.dataManager = FullBatch(trainInputs, trainTargets, validInputs, validTargets);
+   trainer.model = nnet;
+   trainer.reset();
+   trainer.train();
+   outputs(validIdx) = nnet.output(validInputs);
+   fprintf('\n');
 end
 
 %% Determine threshold value that yields the best score
 outputs = gather(outputs);
 targets(targets == -1) = 0;
-for j = 1:N
-   event1 = Event1Score(outputs(j,:), targets);
-   lemeshow = Lemeshow(outputs(j,:), targets);
-   scores(j,:) = [event1, lemeshow]; %#ok<SAGROW>
-
-   fprintf('\n\n Max Score: %.4f \t Lemeshow %.4f \n', ...
+event1 = Event1Score(outputs, targets);
+lemeshow = Lemeshow(outputs, targets);
+fprintf('\n\n Max Score: %.4f \t Lemeshow %.4f \n', ...
               event1, lemeshow);
-end
 
-% means = mean(outputs, 1);
-% means = min(.99, max(.01, means));
-% mean_scores = [Event1Score(means, targets), Lemeshow(means, targets)];
-% fprintf('\n\n Ensemble Scores:    Event1 %.4f    Lemeshow %.4f \n', ...
-%             mean_scores(1), mean_scores(2));
-         
-%% Stack new net on top using outputs as new trianing set
-outputs = normalize(outputs);
-hold_outs = cross_validate_partition(4000, nFolds);
-stacked_outs = gpuState.zeros(1, 4000);
-stacked_nnet = FeedForwardNet('dropout', true, 'inputDropout', 0);
-stacked_nnet.hiddenLayers = {TanhHiddenLayer(N, 100)};
-stacked_nnet.outputLayer = LogisticOutputLayer(100);
-
-trainer = GradientTrainer();
-trainer.stepCalculator = IRprop(.01);
-trainer.reporter = ConsoleReporter();
-trainer.trainingSchedule = BasicMomentumSchedule(.01, 0, 30);
-trainer.model = stacked_nnet;
-
-for foldIdx = 1:nFolds
-   
-   fprintf('\nFold %d Beginning.\n', foldIdx)
-   validIdx = hold_outs{foldIdx};
-   trainIdx = setdiff(1:4000, validIdx);
-   
-   trainInputs = single(gpuArray(outputs(:, trainIdx)));
-   trainTargets = single(gpuArray(targets(:, trainIdx)));
-   validInputs = single(gpuArray(outputs(:, validIdx)));
-   validTargets = single(gpuArray(targets(:, validIdx)));
-   
-   trainer.dataManager = FullBatch(trainInputs, trainTargets, validInputs, validTargets);
-   trainer.reset();
-   trainer.train();
-   stacked_outs(validIdx) = stacked_nnet.output(validInputs);
-   fprintf('\n');
-   
-end
-
-stacked_outs = gather(stacked_outs);
-stackedEvent1 = Event1Score(stacked_outs, targets, true)
-stackedLemeshow = Lemeshow(stacked_outs, targets, true)
