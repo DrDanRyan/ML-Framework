fbc54b66b3b31113dbd2dbd946a8e4b9004ad588
Daniel Ryan <danryanfl@yahoo.com>
2013-09-28 11:12:47 -0400

    Rename hingeExp to lossExponent

--- Layer/SVMOutputLayer.m
+++ Layer/SVMOutputLayer.m
@@ -1,10 +1,10 @@
 classdef SVMOutputLayer < StandardOutputLayer
-   % A linear output layer with a hinge loss function raised to the power hingeExp.
+   % A linear output layer with a hinge loss function raised to the power lossExponent.
    % Requires targets to take values {-1, 1} (as opposed to {0, 1} for LogisticOutputLayer)
    
    properties
       costRatio % multiplies the loss for incorrectly classifying positive (rarer) examples
-      hingeExp % exponent of the hinge loss function (>= 1)
+      lossExponent % exponent of the hinge loss function (>= 1)
       nonlinearity = @(x) x; % not used; Abstract property in StandardLayer
    end
    
@@ -13,19 +13,19 @@ classdef SVMOutputLayer < StandardOutputLayer
          obj = obj@StandardOutputLayer(inputSize, 1, varargin{:});
          p = inputParser();
          p.KeepUnmatched = true;
-         p.addParamValue('hingeExp', 2, @(x) x >= 1)
+         p.addParamValue('lossExponent', 2, @(x) x >= 1)
          p.addParamValue('costRatio', 1);
          parse(p, varargin{:});
-         obj.hingeExp = p.Results.hingeExp;
+         obj.lossExponent = p.Results.lossExponent;
          obj.costRatio = p.Results.costRatio;
       end
          
       function [dLdz, y] = dLdz(obj, x, t)
          y = obj.feed_forward(x);
          if obj.costRatio == 1
-            if obj.hingeExp > 1
-               dLdz = -obj.hingeExp*t.*(max(1 - y.*t, 0).^(obj.hingeExp-1));
-            else % hingeExp == 1
+            if obj.lossExponent > 1
+               dLdz = -obj.lossExponent*t.*(max(1 - y.*t, 0).^(obj.lossExponent-1));
+            else % lossExponent == 1
                dLdz = -t.*obj.gpuState.make_numeric(y.*t < 1);
             end
          else % obj.costRatio ~= 1
@@ -36,11 +36,11 @@ classdef SVMOutputLayer < StandardOutputLayer
             tNeg = t(negIdx);
             yNeg = y(negIdx);
             dLdz = obj.gpuState.zeros(size(y));
-            if obj.hingeExp > 1
-               dLdz(:,posIdx) = -obj.hingeExp*obj.costRatio*...
-                                    tPos.*(max(1 - yPos.*tPos, 0).^(obj.hingeExp-1));
-               dLdz(:,negIdx) = -obj.hingeExp*tNeg.*(max(1 - yNeg.*tNeg, 0).^(obj.hingeExp-1));
-            else % hingeExp == 1
+            if obj.lossExponent > 1
+               dLdz(:,posIdx) = -obj.lossExponent*obj.costRatio*...
+                                    tPos.*(max(1 - yPos.*tPos, 0).^(obj.lossExponent-1));
+               dLdz(:,negIdx) = -obj.lossExponent*tNeg.*(max(1 - yNeg.*tNeg, 0).^(obj.lossExponent-1));
+            else % lossExponent == 1
                dLdz(:,posIdx) = -obj.costRatio*tPos.*obj.gpuState.make_numeric(yPos.*tPos < 1);
                dLdz(:,negIdx) = -tNeg.*obj.gpuState.make_numeric(yNeg.*tNeg < 1);
             end
@@ -55,7 +55,7 @@ classdef SVMOutputLayer < StandardOutputLayer
       
       function loss = compute_loss(obj, y, t)
          if obj.costRatio == 1
-            loss = mean(max(1 - y.*t, 0).^obj.hingeExp);
+            loss = mean(max(1 - y.*t, 0).^obj.lossExponent);
          else
             posIdx = t==1;
             negIdx = t~=1;
@@ -65,8 +65,8 @@ classdef SVMOutputLayer < StandardOutputLayer
             tNeg = t(negIdx);
             yNeg = y(negIdx);
             
-            loss = mean([obj.costRatio*max(1 - yPos.*tPos, 0).^obj.hingeExp, ...
-                           max(1 - yNeg.*tNeg, 0).^obj.hingeExp]);
+            loss = mean([obj.costRatio*max(1 - yPos.*tPos, 0).^obj.lossExponent, ...
+                           max(1 - yNeg.*tNeg, 0).^obj.lossExponent]);
          end
       end
    end
