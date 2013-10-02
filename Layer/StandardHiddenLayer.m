9a4bf2b221da8a5728a43be9239527cf45fcfce3
DrDanRyan <danryanfl@yahoo.com>
2013-10-02 12:56:24 -0400

    Add backprop_with_variance function to StandardLayer

--- Layer/StandardHiddenLayer.m
+++ Layer/StandardHiddenLayer.m
@@ -19,6 +19,25 @@ classdef StandardHiddenLayer < HiddenLayer & StandardLayer
             grad{2} = -mean(dLdz, 2);
          end
       end
+      
+      function [grad, dLdx, gradVariance, nonZeroTerms] = ...
+            backprop_with_variance(obj, x, y, dLdy)
+         % Penalty terms are not used
+         N = size(x, 2);
+         dLdz = dLdy.*obj.dydz(y);
+         dLdx = obj.params{1}'*dLdz;
+         
+         grad{1} = -dLdz*x'/N;
+         grad{2} = -mean(dLdz, 2);
+         
+         gradVariance{1} = (dLdz.^2)*(x.^2)'/N;
+         gradVariance{2} = mean(dLdz.^2, 2);
+         
+         nonZero_dLdz = gpuState.make_numeric(dLdz~=0);
+         nonZero_xTrans = gpuState.make_numeric(x'~=0);
+         nonZeroTerms{1} = nonZero_dLdz*nonZero_xTrans;
+         nonZeroTerms{2} = sum(nonZero_dLdz, 2);
+      end
    end
    
    methods (Abstract)
