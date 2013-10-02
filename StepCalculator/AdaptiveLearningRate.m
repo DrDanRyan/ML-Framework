c4d978078333a81e342097f166d7b07ac6d707ce
DrDanRyan <danryanfl@yahoo.com>
2013-10-02 18:48:31 -0400

    Work on AdaptiveLearningRate

--- StepCalculator/AdaptiveLearningRate.m
+++ StepCalculator/AdaptiveLearningRate.m
@@ -8,6 +8,7 @@ classdef AdaptiveLearningRate < StepCalculator
       eps % small value used to prevent division by zero
       C % constant (>= 1) used to initially overestimate variance and diagonal hessian values
          % in order to slow down learning until estimates are accurate.
+      n0 % number of samples used to initialize moving averages
          
       gradAvg % a moving average of past gradient values
       gradVarAvg % a moving average of past gradVariance values
@@ -23,13 +24,35 @@ classdef AdaptiveLearningRate < StepCalculator
          p = inputParser;
          p.addParamValue('eps', 1e-5);
          p.addParamValue('C', 5);
+         p.addParamValue('n0', 10);
          parse(p, varargin{:});
          
          obj.eps = p.Results.eps;
          obj.C = p.Results.C;
       end
       
+      function take_step(obj, x, t, model, ~)
+         if isempty(obj.gradAvg)
+            N = size(x, 2);
+            idx = randsample(N, obj.n0);
+            obj.initialize_averages(x(:,idx), t(:,idx), model)
+         end
+            
+         
+      end
       
+      function initialize_averages(obj, x, t, model)
+         gra
+      end
+      
+      function reset(obj)
+         obj.gradAvg = [];
+         obj.gradVarAvg = [];
+         obj.hessAvg = [];
+         obj.hessVarAvg = [];
+         obj.learnRates = [];
+         obj.memorySize = [];
+      end
    end
    
 end
