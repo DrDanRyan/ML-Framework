751685ab3bc7d2e705976e0794114fca8952259a
Daniel Ryan <danryanfl@yahoo.com>
2013-10-05 10:15:23 -0400

    Revert DataManager to public properties

--- DataManager/DataManager.m
+++ DataManager/DataManager.m
@@ -1,10 +1,15 @@
 classdef DataManager < matlab.mixin.Copyable
    % This defines the DataManager interface
    
+   properties
+      trainingInputs
+      trainingTargets
+      validationInputs
+      validationTargets
+   end
+   
    methods (Abstract)
       [x, t, endOfEpochFlag] = next_batch(obj)
-      [inputs, targets] = get_validation_data(obj)
-      [inputs, targets] = get_training_data(obj)
       reset(obj)
    end
    
