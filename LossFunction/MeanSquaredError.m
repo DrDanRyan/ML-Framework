cc4fdd215f95c776a10cd47225c0419e364ecca2
Daniel Ryan <danryanfl@yahoo.com>
2013-10-13 09:38:28 -0400

    Add factor of .5 to loss

--- LossFunction/MeanSquaredError.m
+++ LossFunction/MeanSquaredError.m
@@ -2,11 +2,11 @@ classdef MeanSquaredError < LossFunction
    
    methods
       function dLdy = dLdy(obj, y, t)
-         dLdy = 2*(y - t);
+         dLdy = y - t;
       end
       
       function loss = compute_loss(obj, y, t)
-         loss = mean(mean((y - t).^2));
+         loss = .5*mean(mean((y - t).^2));
       end
    end
    
