f150c7f5c34e3de3dfa7dd0bc07d27f55a090ae0
DrDanRyan <danryanfl@yahoo.com>
2013-10-17 18:30:28 -0400

    no change

--- functions/ffn_from_aes.m
+++ functions/ffn_from_aes.m
@@ -1,10 +1,14 @@
-function ffn = ffn_from_aes(aes, varargin)
-ffn = FeedForwardNet(varargin{:});
+function ffn = ffn_from_aes(aes, inputDropout, hiddenDropout)
+ffn = FeedForwardNet('inputDropout', inputDropout, ...
+                     'hiddenDropout', hiddenDropout);
 nAE = length(aes);
 ffn.hiddenLayers = cell(1, 2*nAE - 1);
 
-for i = 1:nAE
+
+
+for 2 = 1:nAE
    ffn.hiddenLayers{i} = aes{i}.encodeLayer.copy();
+   ffn.hiddenLayers{i}.params{1} = ffn.hiddenLayers{i}.params{1}/(1-hiddenDropout);
 end
 
 % Copy the hiddenLayer portion of the decodeLayer (assumes ComboOutputLayer type)
