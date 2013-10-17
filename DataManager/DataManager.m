769179c397f114243ab656a5adb79b1611ad2b13
DrDanRyan <danryanfl@yahoo.com>
2013-10-17 13:20:41 -0400

    Remove noise injection

--- DataManager/DataManager.m
+++ DataManager/DataManager.m
@@ -12,10 +12,6 @@ classdef DataManager < matlab.mixin.Copyable
       stopIdx
    end
    
-   properties (Dependent)
-      isGPU
-   end
-   
    methods
       function obj = DataManager(trainingInputs, trainingTargets, validationInputs, ...
                                        validationTargets, varargin)
@@ -27,7 +23,6 @@ classdef DataManager < matlab.mixin.Copyable
          
          p = inputParser();
          p.addParamValue('batchSize', []);
-         p.addParamValue('noise', []);
          parse(p, varargin{:});
          
          if ~isempty(p.Results.batchSize)
@@ -39,22 +34,18 @@ classdef DataManager < matlab.mixin.Copyable
             end
             obj.shuffle_training_data();
          end
-         
-         if ~isempty(p.Results.noise)
-            obj.noiseLevel = p.Results.noise;
-         end
       end
       
       function [x, t, endOfEpochFlag] = next_batch(obj)
          if isempty(obj.batchSize) % full batch
-            x = obj.inject_noise(obj.trainingInputs);
+            x = obj.trainingInputs;
             t = obj.trainingTargets;
             endOfEpochFlag = true;
          else % mini-batch
             if isempty(obj.trainingInputs)
                x = [];
             else
-               x = obj.inject_noise(obj.trainingInputs(:, obj.startIdx:obj.stopIdx));
+               x = obj.trainingInputs(:, obj.startIdx:obj.stopIdx);
             end
             
             if isempty(obj.trainingTargets)
@@ -79,22 +70,6 @@ classdef DataManager < matlab.mixin.Copyable
                                                              obj.trainingTargets);
          obj.startIdx = 1;
          obj.stopIdx = min(obj.batchSize, obj.trainingSize);
-      end
-      
-      function isGPU = get.isGPU(obj)
-         isGPU = isa(obj.trainingInputs, 'gpuArray');
-      end
-      
-      function x = inject_noise(obj, x)
-         if isempty(obj.noiseLevel)
-            return
-         end
-         
-         if obj.isGPU
-            x = x + obj.noiseLevel*gpuArray.randn(size(x), 'single');
-         else
-            x = x + obj.noiseLevel*randn(size(x));
-         end
       end
       
       function reset(obj)
