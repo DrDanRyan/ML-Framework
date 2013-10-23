e9b603a7067d630b3b82b67a6f5bc35feea39e7f
Daniel Ryan <danryanfl@yahoo.com>
2013-10-23 17:14:14 -0400

    Revise NaN handling

--- Layer/LogisticOutputLayer.m
+++ Layer/LogisticOutputLayer.m
@@ -17,7 +17,7 @@ classdef LogisticOutputLayer < StandardOutputLayer
    
       function loss = compute_loss(~, y, t)
          % loss = mean(-t.*log(y) - (1-t).*log(1-y));
-         loss = (sum(-log(y(t==1))) + sum(-log(1-y(t==0))))/length(t); % more robust to NaN from
+         loss = (sum(-log(y(t==1))) + sum(-log(1-y(t==0))))/length(t(~isnan(t))); % more robust to NaN from
                                                                        % 0*(-Inf) when t == y == 0
                                                                        % or t == y == 1              
       end
