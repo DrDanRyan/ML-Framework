e40d0916c759cbec45c65bc1541a1869782efbdf
Daniel Ryan <danryanfl@yahoo.com>
2013-10-25 23:58:52 -0400

    end of night

--- AutoEncoder/CAE.m
+++ AutoEncoder/CAE.m
@@ -13,10 +13,10 @@ classdef CAE < AutoEncoder
          obj = obj@AutoEncoder(varargin{:});
          p = inputParser;
          p.KeepUnmatched = true;
-         p.addParamValue('JacCoeff', []);
+         p.addParamValue('JacCoeff', .1);
          p.addParamValue('HessCoeff', []);
          p.addParamValue('HessNoise', .1);
-         p.addParamValue('HessBatchSize', 20);
+         p.addParamValue('HessBatchSize', 10);
          parse(p, varargin{:});
          
          obj.JacCoeff = p.Results.JacCoeff;
@@ -29,8 +29,8 @@ classdef CAE < AutoEncoder
          xIn(isnan(xIn)) = 0;
          xCode = obj.encodeLayer.feed_forward(xIn);
          [decodeGrad, dLdxCode, xRecon] = obj.decodeLayer.backprop(xCode, xTarget);
-         [encodeGrad, ~, dydz] = obj.encodeLayer.backprop(xIn, xCode, dLdxCode);
-         penalty = obj.compute_contraction_penalty_gradient(xIn, dydz);
+         [encodeGrad, ~, Dy] = obj.encodeLayer.backprop(xIn, xCode, dLdxCode);
+         penalty = obj.compute_contraction_penalty_gradient(xIn, xCode, Dy);
          encodeGrad = cellfun(@(grad, pen) grad + pen, encodeGrad, penalty, ...
                                  'UniformOutput', false);
          
@@ -46,21 +46,36 @@ classdef CAE < AutoEncoder
          end
       end
       
-      function penalty = compute_contraction_penalty_gradient(obj, xIn, dydz)
-         dydzSqMean = mean(dydz.*dydz, 2); % L2 x 1 (x k)
-         penalty = obj.JacCoeff*bsxfun(@times, obj.encodeLayer.params{1}, dydzSqMean);
+      function penalty = compute_contraction_penalty_gradient(obj, xIn, xCode, Dy)
+         [L1, N] = size(xIn);
+         penalty = cell(1, 2);
+         W = obj.encodeLayer.params{1};
          
+         % Penalty from L2 norm of Jacobian map
+         W_RowL2 = sum(W.*W, 2);
+         D2y = obj.encodeLayer.compute_D2y(xIn, xCode);
+         Dy_D2y_product = Dy.*D2y;
+         penalty{2} = obj.JacCoeff*W_RowL2.*mean(Dy_D2y_product, 2);
+         
+         if ndims(Dy) <= 2
+            x_outer_DyD2y = Dy_D2y_product*xIn'/N;
+         else % ndims == 3 => Maxout layer
+            x_outer_DyD2y = pagefun(@mtimes, Dy_D2y_product, x')/N;
+         end
+         penalty{1} = obj.JacCoeff*(bsxfun(@times, W_RowL2, x_outer_DyD2y) + ...
+                                    bsxfun(@times, W, mean(Dy.*Dy, 2)));
+         
+
+         % Penalty from approximation to L2 norm of Hessian map
          if ~isempty(obj.HessCoeff)
-            [L1, N] = size(xIn);
-            permvec = randperm(N, obj.HessBatchSize);
-            dydz = dydz(:,permvec);
-            xIn2 = xIn(:,premvec) + obj.HessNoise*obj.gpuState.randn([L1, obj.HessBatchSize]);
-            xCode2 = obj.encodeLayer.feed_forward(xIn2);
-            dydz2= obj.encodeLayer.compute_dydz(xIn2, xCode2);
-            diff = dydz - dydz2;
-            diffSqMean = mean(diff.*diff, 2);
-            penalty = penalty + obj.HessCoeff*bsxfun(@times, obj.encodeLayer.params{1}, ...
-                                                        diffSqMean);
+            xEps = bsxfun(@plus, xIn, ...
+                           obj.HessNoise*obj.gpuState.randn([size(xIn), obj.HessBatchSize]));
+            xEpsLong = reshape(xEps, L1, []);
+            xCodeEps = obj.encodeLayer.feed_forward(xEpsLong);
+            DyEps = obj.encodeLayer.compute_Dy(xEpsLong, xCodeEps);
+            clear xEpsLong
+            xCodeEps = reshape(xCodeEps, [L2, N, obj.HessBatchSize]);
+            DyEps = reshape(DyEps, L2, N, obj.HessBatchSize, []);
          end    
       end
       
