8e2fda6e7eafa19746d4ae09522e6f1fa0c6c1e1
Daniel Ryan <danryanfl@yahoo.com>
2013-11-08 09:59:40 -0500

    finish MomentumSchedule

--- TrainingSchedule/MomentumSchedule.m
+++ TrainingSchedule/MomentumSchedule.m
@@ -21,7 +21,7 @@ classdef MomentumSchedule < TrainingSchedule
    end
    
    methods
-      function obj = MomentumSchedule(fastEpochs, lr0, maxMomentum, varargin{:})
+      function obj = MomentumSchedule(fastEpochs, lr0, maxMomentum, varargin)
          p = inputParser();
          p.addParamValue('lr0', []);
          p.addParamValue('lrDecay', []);
@@ -49,21 +49,30 @@ classdef MomentumSchedule < TrainingSchedule
       
       function isContinue = update(obj, ~, ~, ~)
          obj.epoch = obj.epoch + 1;
-         if obj.epoch < obj.fastEpochs
+         if obj.epoch < obj.fastEpochs % Fast regime
             isContinue = true;
             obj.params{2} = min(obj.maxMomentum, (obj.epoch + obj.C)/(obj.epoch + 2*obj.C));
             if obj.epoch >= obj.lrBurnIn && ~isempty(obj.lrDecay)
                obj.params{1} = obj.params{1}*obj.lrDecay;
             end
-         elseif obj.epoch < obj.fastEpochs + obj.slowEpochs
+         elseif obj.epoch < obj.fastEpochs + obj.slowEpochs % Slow regime
+            isContinue = true;
+            if ~isempty(obj.slowMomentum)
+               obj.params{2} = obj.slowMomentum;
+            end
             
-         else
+            if ~isempty(obj.slowLearnRate)
+               obj.params{1} = obj.slowLearnRate;
+            end            
+         else % all iterations completed
             isContinue = false;
          end         
       end
       
       function reset(obj)
-         
+         obj.params{1} = obj.lr0;
+         obj.params{2} = min(obj.maxMomentum, (1+obj.C)/(1+2*obj.C));
+         obj.epoch = 0;
       end
    end
    
