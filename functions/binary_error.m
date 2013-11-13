32f1cefdd5e257e0d41c99c4ed4efe40465b53a4
Daniel Ryan <danryanfl@yahoo.com>
2013-11-13 09:31:51 -0500

    Add binary error functions and rename multiclass_error functions

---
+++ functions/binary_error.m
@@ -0,0 +1,13 @@
+function error_rate = binary_error(y, t)
+
+if any(t < 0) % svm targets
+   threshold = 0;
+   t(t < 0) = 0; % change to {0, 1} targets
+else % logistic targets
+   threshold = .5;
+end
+
+predictions = y > threshold;
+error_rate = sum(predictions ~= t)/N;
+end
+
