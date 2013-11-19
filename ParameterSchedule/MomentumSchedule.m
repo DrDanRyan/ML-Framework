ca22842c2fa3b177ff1697f97fda6b4d65a83189
Daniel Ryan <danryanfl@yahoo.com>
2013-11-19 10:30:05 -0800

    changed Momentum scheme to accept layerwise and componentwise parameters

--- ParameterSchedule/MomentumSchedule.m
+++ ParameterSchedule/MomentumSchedule.m
@@ -11,40 +11,69 @@ classdef MomentumSchedule < ParameterSchedule
       C
       maxMomentum
       
+      % Keep track of number of updates that have passed
       nUpdates = 0 
    end
    
    methods
-      function obj = MomentumSchedule(lr0, maxMomentum, varargin)
-         obj.params = {lr0, maxMomentum};
-         obj.lr0 = lr0;
-         obj.maxMomentum = maxMomentum;
-         
+      function obj = MomentumSchedule(lr0, maxMomentum, varargin) 
          p = inputParser();
          p.addParamValue('lrHalfLife', []);
          p.addParamValue('lrBurnIn', 0);
          p.addParamValue('C', []);
+         p.addParamValue('gradShape', []); % used to specify lr0 and maxMomentum for each LAYER of network
          parse(p, varargin{:});
          
+         obj.lr0 = lr0;
+         obj.maxMomentum = maxMomentum;
+         obj.params = {lr0, maxMomentum};
          obj.lrDecay = exp(log(.5)/p.Results.lrHalfLife);
          obj.lrBurnIn = p.Results.lrBurnIn;
          obj.C = p.Results.C;
+         gradShape = p.Results.gradShape;
+         
+         %If parameters are specified layerwise, unroll layerwise specs
+         %into componentwise specs using gradShape
+         if ~isempty(obj.gradShape)
+            startIdx = 1;
+            obj.lr0 = cell(1, sum(obj.gradShape));
+            obj.maxMomentum = cell(1, sum(obj.gradShape));
+            for i = 1:length(gradShape);
+               stopIdx = startIdx + obj.gradShape(i) - 1;
+               dummy = ones(1, gradShape(i));
+               obj.lr0(startIdx:stopIdx) = mat2cell(lr0*dummy, 1, dummy);
+               obj.maxMomentum(startIdx:stopIdx) = mat2cell(maxMomentum*dummy, 1, dummy);
+               startIdx = stopIdx + 1;
+            end
+            obj.params = {obj.lr0, obj.maxMomentum};
+         end
       end
       
       function params = update(obj)
          obj.nUpdates = obj.nUpdates + 1;
-         
-         % Decay learning rate if appropriate
-         if ~isempty(obj.lrDecay) && obj.nUpdates > obj.lrBurnIn
-            obj.params{1} = obj.lrDecay*obj.params{1};
+
+         if isa(obj.lr0, 'cell')
+            % Decay learning rate if appropriate
+            if ~isempty(obj.lrDecay) && obj.nUpdates > obj.lrBurnIn
+               obj.params{1} = cellfun(@(lr) obj.lrDecay*lr, obj.params{1}, 'UniformOutput', false);
+            end
+            % Set momentum
+            if ~isempty(obj.C)
+               obj.params{2} = cellfun(@(m) min(m, (obj.nUpdates + obj.C)/(obj.nUpdates + 2*obj.C)), ...
+                                          obj.maxMomentum, 'UniformOutput', false);
+            end
+         else % assume lr0 is a scalar, same params for all of grad
+            % Decay learning rate if appropriate
+            if ~isempty(obj.lrDecay) && obj.nUpdates > obj.lrBurnIn
+               obj.params{1} = obj.lrDecay*obj.params{1};
+            end
+            % Set momentum
+            if ~isempty(obj.C)
+               obj.params{2} = min(obj.maxMomentum, ...
+                                       (obj.nUpdates + obj.C)/(obj.nUpdates + 2*obj.C));
+            end
          end
-         
-         % Set momentum
-         if ~isempty(obj.C)
-            obj.params{2} = min(obj.maxMomentum, ...
-                                    (obj.nUpdates + obj.C)/(obj.nUpdates + 2*obj.C));
-         end
-         
+
          params = obj.params;
       end
       
--- StepCalculator/NesterovMomentum.m
+++ StepCalculator/NesterovMomentum.m
@@ -13,20 +13,33 @@ classdef NesterovMomentum < StepCalculator
    
    methods
       function take_step(obj, batch, model, params)
-         [learnRate, momentum] = params{:}; % learnRate and momentum are each cell arrays
-                                            % they can be different for each component of grad
+         [learnRate, momentum] = params{:}; % learnRate and momentum are either scalars or cell 
+                                            % arrays the same size as grad
          
          if isempty(obj.velocity)
             grad = model.gradient(batch);
-            obj.velocity = cellfun(@(lr, grad) -lr*grad, learnRate, grad, 'UniformOutput', false);
+            if isscalar(learnRate)
+               obj.velocity = cellfun(@(grad) -learnRate*grad, grad, 'UniformOutput', false);
+            else
+               obj.velocity = cellfun(@(lr, grad) -lr*grad, learnRate, grad, 'UniformOutput', false);
+            end
          else
             modelCopy = model.copy();
-            modelCopy.increment_params(cellfun(@(m, v) m*v, momentum, obj.velocity, ...
-                                             'UniformOutput', false));
-            grad = modelCopy.gradient(batch);
-            clear modelCopy
-            obj.velocity = cellfun(@(lr, m, grad, vel) m*vel - lr*grad, learnRate, momentum, ...
-                                             grad, obj.velocity, 'UniformOutput', false);   
+            if isscalar(learnRate)
+               modelCopy.increment_params(cellfun(@(v) momentum*v, obj.velocity, ...
+                                                 'UniformOutput', false));
+               grad = modelCopy.gradient(batch);
+               clear modelCopy
+               obj.velocity = cellfun(@(grad, vel) momentum*vel - learnRate*grad, grad, ...
+                                                obj.velocity, 'UniformOutput', false);
+            else
+               modelCopy.increment_params(cellfun(@(m, v) m*v, momentum, obj.velocity, ...
+                                                'UniformOutput', false));
+               grad = modelCopy.gradient(batch);
+               clear modelCopy
+               obj.velocity = cellfun(@(lr, m, grad, vel) m*vel - lr*grad, learnRate, momentum, ...
+                                                grad, obj.velocity, 'UniformOutput', false);   
+            end
          end
          model.increment_params(obj.velocity);
       end
