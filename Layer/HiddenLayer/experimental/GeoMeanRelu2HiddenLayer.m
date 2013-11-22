389931c87310cfa157a4123caa0c0bdd9727a793
DrDanRyan <danryanfl@yahoo.com>
2013-11-22 14:00:43 -0500

    add GeoMeanRelu2 layer

---
+++ Layer/HiddenLayer/experimental/GeoMeanRelu2HiddenLayer.m
@@ -0,0 +1,53 @@
+classdef GeoMeanRelu2HiddenLayer < StandardLayer & HiddenLayer
+   
+   properties
+      D
+      % parmas = {W, b} with W ~ L2 x L1 x D and b ~ L2 x 1 x D
+      isLocallyLinear = false
+   end
+   
+   methods
+      function obj = GeoMeanRelu2HiddenLayer(inputSize, outputSize, D, varargin)
+         obj = obj@StandardLayer(inputSize, outputSize, varargin{:});
+         obj.D = D;
+         obj.init_params();
+      end
+      
+      function init_params(obj)
+         obj.params{2} = .1*obj.gpuState.ones(obj.outputSize, 1, obj.D);
+         for d = 1:obj.D
+            obj.params{1}(:,:,d) = matrix_init(obj.outputSize, obj.inputSize, obj.initType, ...
+                                                      obj.initScale, obj.gpuState);
+         end
+      end
+      
+      function [y, zRect] = feed_forward(obj, x)
+         z = obj.compute_z(x);
+         zRect = max(0, z);
+         y = prod(zRect, 3).^(1/obj.D);
+      end
+      
+      function value = compute_Dy(obj, zRect, y)
+         value = bsxfun(@rdivide, y, obj.D*zRect);
+         value(isnan(value)) = 0;
+      end
+      
+      function value = compute_D2y(obj, ffExtras, y, Dy)
+         % pass
+      end
+      
+      function value = compute_z(obj, x)
+         value = bsxfun(@plus, pagefun(@mtimes, obj.params{1}, x), obj.params{2});
+      end
+      
+      function [grad, dLdx, Dy] = backprop(obj, x, y, zRect, dLdy)
+         Dy = obj.compute_Dy(zRect, y);
+         dLdz = bsxfun(@times, dLdy, Dy);
+         dLdx = sum(pagefun(@mtimes, permute(obj.params{1}, [2 1 3]), dLdz), 3);
+         grad{1} = pagefun(@mtimes, dLdz, x')/size(x, 2); % L2 x L1 x D
+         grad{2} = mean(dLdz, 2); % L2 x 1 x D
+      end
+   end
+   
+end
+
--- tests/geo_mean.m
+++ tests/geo_mean.m
@@ -1,16 +1,16 @@
 clear all
 figure()
-[z1, z2] = meshgrid(-10:.1:10, -10:.1:10);
-y1 = 1./(1 + exp(-z1));
-y2 = 1./(1 + exp(-z2));
+[z1, z2] = meshgrid(-20:.5:20, -20:.5:20);
+y1 = max(0, z1);
+y2 = max(0, z2);
 
-F1 = (y1.^(4/3).*y2.^(2/3)).^(1/2);
+F1 = exp(.5*(log(y1) + log(y2)));
 surf(z1, z2, F1)
-%contourf(z1, z2, F1, 11, 'ShowText', 'on')
+%contourf(z1, z2, F1, 41, 'ShowText', 'on')
 
-figure()
-y1 = min(0, z1);
-y2 = min(0, z2);
-F2 = exp((4*y1/3 + 2*y2/3)/2);
-%contourf(z1, z2, F2, 11, 'ShowText', 'on')
-surf(z1, z2, F2)
+% figure()
+% y1 = min(0, z1);
+% y2 = min(0, z2);
+% F2 = exp((4*y1/3 + 2*y2/3)/2);
+% %contourf(z1, z2, F2, 11, 'ShowText', 'on')
+% surf(z1, z2, F2)
