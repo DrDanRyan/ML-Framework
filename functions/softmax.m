7e91a777f2652ce6c4f91eba2752cf1e1b3e31a1
Daniel Ryan <danryanfl@yahoo.com>
2013-11-22 10:15:26 -0800

    fixed error in softmax

--- functions/softmax.m
+++ functions/softmax.m
@@ -1,6 +1,6 @@
 function y = softmax(z)
 
-z = bsxfun(@minus, z, max(z, 1)); % rescale incoming activations so max is 0
+z = bsxfun(@minus, z, max(z, [], 1)); % rescale incoming activations so max is 0
 yHat = exp(z);
 ySum = sum(yHat, 1);
 y = bsxfun(@rdivide, yHat, ySum); % rescale outputs so they sum to 1
