0efa43d8bf9ac8c4f3fc727819b49a208ca4556e
Daniel Ryan <danryanfl@yahoo.com>
2013-12-05 21:44:07 -0500

    add burnIn property

--- ProgressMonitor/EarlyStopping.m
+++ ProgressMonitor/EarlyStopping.m
@@ -3,18 +3,21 @@ classdef EarlyStopping < BasicMonitor
    properties
       bufferConst % number of validation intervals to continue without improvement before quitting (should be integer >= 0)
       bufferMult  % should be real >= 1
+      burnIn
    end
    
    methods
-      function obj = EarlyStopping(bufferConst, bufferMult, varargin)
+      function obj = EarlyStopping(bufferConst, bufferMult, burnIn, varargin)
          obj = obj@BasicMonitor(varargin{:});
          obj.bufferConst = bufferConst;
          obj.bufferMult = bufferMult;
+         obj.burnIn = burnIn;
       end
       
       function isContinue = should_continue(obj)
-         isContinue = obj.nUpdates < floor(obj.bufferConst + ...
-                obj.bufferMult*obj.bestUpdate/obj.validationInterval)*obj.validationInterval;
+         isContinue = (obj.nUpdates < floor(obj.bufferConst + ...
+                obj.bufferMult*obj.bestUpdate/obj.validationInterval)*obj.validationInterval) | ...
+                (obj.nUpdates < obj.burnIn*obj.validationInterval);
       end
    end
    
