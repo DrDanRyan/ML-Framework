8148a9f1fb7e5b631595e760455a998d9ec14eb7
Daniel Ryan <danryanfl@yahoo.com>
2013-12-13 14:04:33 -0500

    add sparse filtering loss function to fit with ffn

---
+++ LossFunction/SparseFilteringLoss.m
@@ -0,0 +1,31 @@
+classdef SparseFilteringLoss < LossFunction
+   
+   properties
+   end
+   
+   methods
+      function dLdy = dLdy(~, y, ~)
+         rowNorms = sqrt(sum(y.*y, 2));
+         yRowNormed = bsxfun(@rdivide, y, rowNorms);
+         colNorms = sqrt(sum(yRowNormed.*yRowNormed, 1));
+         F = bsxfun(@rdivide, yRowNormed, colNorms);
+
+         % Backprop
+         dLdy = sign(F);
+         dLdy = bsxfun(@rdivide, dLdy, colNorms) ...
+                     - bsxfun(@times, F, sum(dLdy.*yRowNormed, 1)./(colNorms.*colNorms));       
+         dLdy = bsxfun(@rdivide, dLdy, rowNorms) ...
+                     - bsxfun(@times, yRowNormed, sum(dLdy.*y, 2)./(rowNorms.*rowNorms));
+      end
+      
+      function loss = compute_loss(~, y, ~)
+         rowNorms = sqrt(sum(y.*y, 2));
+         yRowNormed = bsxfun(@rdivide, y, rowNorms);
+         colNorms = sqrt(sum(yRowNormed.*yRowNormed, 1));
+         F = bsxfun(@rdivide, yRowNormed, colNorms);
+         loss = sum(abs(F(:)));
+      end
+   end
+   
+end
+
