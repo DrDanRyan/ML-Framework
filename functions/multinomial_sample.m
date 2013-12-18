3b48a8a2b152862970370f2f75cb158faebf2dc9
Daniel Ryan <danryanfl@yahoo.com>
2013-12-17 19:26:39 -0500

    default dimension of 1

--- functions/multinomial_sample.m
+++ functions/multinomial_sample.m
@@ -1,24 +1,26 @@
 function sample = multinomial_sample(probArray, dim)
-% probArray is a k x N matrix where each column is a probability vector
-% Function returns a k x N matrix of a single multinomial sample
-    sampleSize = size(probArray);
-    sampleSize(dim) = 1;
-    isGPU = isa(probArray, 'gpuArray');
-    
-    if isGPU
-       randProb = gpuArray.rand(sampleSize, 'single');
-       padding = gpuArray.ones(sampleSize,'single');
-    else
-       randProb = rand(sampleSize);
-       padding = ones(sampleSize);
-    end
-    
-    probArray = cumsum(probArray, dim);
-    if isGPU
-       isBelowRand = cat(dim, padding, single(bsxfun(@lt, probArray, randProb)));
-    else
-       isBelowRand = cat(dim, padding, bsxfun(@lt, probArray, randProb));
-    end
-    sample = -diff(isBelowRand, 1, dim);
+   if nargin < 2
+      dim = 1;
+   end
+   
+   sampleSize = size(probArray);
+   sampleSize(dim) = 1;
+   isGPU = isa(probArray, 'gpuArray');
+
+   if isGPU
+    randProb = gpuArray.rand(sampleSize, 'single');
+    padding = gpuArray.ones(sampleSize,'single');
+   else
+    randProb = rand(sampleSize);
+    padding = ones(sampleSize);
+   end
+
+   probArray = cumsum(probArray, dim);
+   if isGPU
+    isBelowRand = cat(dim, padding, single(bsxfun(@lt, probArray, randProb)));
+   else
+    isBelowRand = cat(dim, padding, bsxfun(@lt, probArray, randProb));
+   end
+   sample = -diff(isBelowRand, 1, dim);
 end
 
