de9c265b5c9dd3c57a91541754a01288806f5c16
Daniel Ryan <danryanfl@yahoo.com>
2013-12-20 18:24:29 -0500

    change default relu initScale factor

--- Layer/ConvLayer/Conv1DLayer.m
+++ Layer/ConvLayer/Conv1DLayer.m
@@ -26,7 +26,7 @@ classdef Conv1DLayer < ParamsFunctions & ConvLayer & matlab.mixin.Copyable
          obj.params{1} = 2*obj.initScale*obj.gpuState.rand(obj.nFilters, 1, ...
                                  obj.nChannels, obj.filterSize) - obj.initScale;
          if strcmp(obj.initType, 'relu')
-            obj.params{2} = obj.initScale*obj.gpuState.ones(obj.nFilters, 1);
+            obj.params{2} = .25*obj.initScale*obj.gpuState.ones(obj.nFilters, 1);
          else
             obj.params{2} = obj.gpuState.zeros(obj.nFilters, 1);
          end
