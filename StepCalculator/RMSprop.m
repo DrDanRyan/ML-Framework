42e72089c9df9c05c38dffbbe1766f5aeb69dee1
Daniel Ryan <danryanfl@yahoo.com>
2013-12-24 11:44:25 -0500

    add RMSprop

---
+++ StepCalculator/RMSprop.m
@@ -0,0 +1,33 @@
+classdef RMSprop < StepCalculator
+   
+   properties
+      tau % time constant for geometric moving average
+      gradSquaredAvg % a geometric moving average of gradient squared
+   end
+   
+   methods
+      function obj = RMSprop(tau)
+         obj.tau = tau;
+      end
+      
+      function take_step(obj, batch, model, params)
+         lr = params{1};
+         grad = model.gradient(batch);
+         if isempty(obj.gradSquaredAvg)
+            obj.gradSquaredAvg = cellfun(@(g) g.*g, grad, 'UniformOutput', false);
+         else
+            obj.gradSquaredAvg = cellfun(@(g, avg) obj.tau*avg + (1-obj.tau)*g.*g, ...
+                                             grad, obj.gradSquaredAvg, 'UniformOutput', false);
+         end
+         step = cellfun(@(g, avg) -lr*g./sqrt(avg), grad, obj.gradSquaredAvg, ...
+                           'UniformOutput', false);
+         model.increment_params(step);
+      end
+      
+      function reset(obj)
+         obj.gradSquaredAvg = [];
+      end
+   end
+   
+end
+
