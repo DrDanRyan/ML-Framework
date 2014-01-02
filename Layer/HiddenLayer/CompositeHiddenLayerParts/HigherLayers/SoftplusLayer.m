5c831e94cb8f645da92bcad31e15c7af5eabed31
Daniel Ryan <danryanfl@yahoo.com>
2014-01-02 11:34:25 -0500

    add scaling constant

--- Layer/HiddenLayer/CompositeHiddenLayerParts/HigherLayers/SoftplusLayer.m
+++ Layer/HiddenLayer/CompositeHiddenLayerParts/HigherLayers/SoftplusLayer.m
@@ -1,29 +1,32 @@
 classdef SoftplusLayer < matlab.mixin.Copyable
    
    properties
+      C   % y = log(1 + exp(C*x))/C ~ max(0, x) (larger C leads to closer approx)
       dydx
    end
    
    methods
+      function obj = SoftplusLayer(C)
+         if nargin < 1
+            C = 5;
+         end
+         obj.C = C;
+      end
+      
       function y = feed_forward(obj, x, isSave)
          if nargin == 3 && isSave
-            expX = exp(x);
-            one_plus_expX = 1 + expX;
-            y = log(one_plus_expX);
-            obj.dydx = max(expX./one_plus_expX, 1e-14);
+            expCX = exp(obj.C*x);
+            one_plus_expCX = 1 + expCX;
+            y = log(one_plus_expCX)/obj.C;
+            obj.dydx = expCX./one_plus_expCX;
          else
-            y = log(1 + exp(x));
+            y = log(1 + exp(obj.C*x))/obj.C;
          end
          
          % replace degenerate values
          yInf = y == Inf;
-         yZero = y == 0;
          if any(yInf(:))
             y(yInf) = x(yInf);
-         end
-         
-         if any(yZero(:))
-            y(yZero) = 1e-14;
          end
       end
       
