ea256ff20f8909d1c2b161f107348d3f4bbe68ed
Daniel Ryan <danryanfl@yahoo.com>
2014-01-28 16:37:20 -0500

    Removed positivity assumption

--- LossFunction/SparseFilteringLoss.m
+++ LossFunction/SparseFilteringLoss.m
@@ -1,46 +1,26 @@
 classdef SparseFilteringLoss < LossFunction
-   % Assumes output is non-negative. Fudges all y values by adding eps
-   % to prevent degenericies arising from all examples of a feature == 0
-
-   properties
-      eps
-   end
    
-   methods
-      function obj = SparseFilteringLoss(eps)
-         if nargin < 1
-            eps = 1e-4;
-         end
-         obj.eps = eps;
-      end
-      
-      function dLdy = dLdy(obj, y, ~)
-         y = y + obj.eps;
+   methods      
+      function dLdy = dLdy(~, y, ~)
          rowNorms = sqrt(sum(y.*y, 2));
          yRowNormed = bsxfun(@rdivide, y, rowNorms);
          colNorms = sqrt(sum(yRowNormed.*yRowNormed, 1));
          F = bsxfun(@rdivide, yRowNormed, colNorms);
          
          % Backprop
-         if isa(F, 'gpuArray')
-            dLdy = gpuArray.ones(size(F), 'single');
-         else
-            dLdy = ones(size(F));
-         end
-         
+         dLdy = sign(F);         
          dLdy = bsxfun(@rdivide, dLdy, colNorms) ...
                      - bsxfun(@times, F, sum(dLdy.*yRowNormed, 1)./(colNorms.*colNorms));       
          dLdy = bsxfun(@rdivide, dLdy, rowNorms) ...
                      - bsxfun(@times, yRowNormed, sum(dLdy.*y, 2)./(rowNorms.*rowNorms));
       end
       
-      function loss = compute_loss(obj, y, ~)
-         y = y + obj.eps;
+      function loss = compute_loss(~, y, ~)
          rowNorms = sqrt(sum(y.*y, 2));
          yRowNormed = bsxfun(@rdivide, y, rowNorms);
          colNorms = sqrt(sum(yRowNormed.*yRowNormed, 1));
          F = bsxfun(@rdivide, yRowNormed, colNorms);
-         loss = sum(F(:));
+         loss = mean(sum(abs(F), 1), 2);
       end
       
    end
