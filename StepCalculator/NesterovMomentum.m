73574dbf6d7a33df39ccaf92129a1a0179fb418e
DrDanRyan <danryanfl@yahoo.com>
2014-02-24 13:36:04 -0500

    Refactor take_step

--- StepCalculator/NesterovMomentum.m
+++ StepCalculator/NesterovMomentum.m
@@ -12,36 +12,49 @@ classdef NesterovMomentum < StepCalculator
    end
    
    methods
-      function take_step(obj, batch, model, params)
-         [lr, rho] = params{:}; % lr (learning rate) and rho (momentum) are either 
-                                % scalars or cell arrays the same size as grad
+      function take_step(obj, batch, model, params)                               
+         if ~isempty(obj.velocity)
+            model.increment_params(obj.compute_first_step(params));
+         end
          
+         grad = model.gradient(batch);
+         model.increment_params(obj.compute_second_step(params, grad));
+      end
+      
+      function step = compute_first_step(obj, params)
+         rho = params{2};
+         if isscalar(rho)
+            step = cellfun(@(vel) rho*vel, obj.velocity, 'UniformOutput', false);
+         else
+            step = cellfun(@(rho, vel) rho*vel, rho, obj.velocity, ...
+                           'UniformOutput', false);
+         end
+      end
+      
+      function step = compute_second_step(obj, params, grad)
+         [lr, rho] = params{:};
          if isempty(obj.velocity)
-            grad = model.gradient(batch);
             if isscalar(lr)
-               obj.velocity = cellfun(@(grad) -lr*grad, grad, 'UniformOutput', false);
+               obj.velocity = cellfun(@(grad) -lr*grad, grad, ...
+                                      'UniformOutput', false);
             else
-               obj.velocity = cellfun(@(lr, grad) -lr*grad, lr, grad, 'UniformOutput', false);
-            end
-            model.increment_params(obj.velocity);
+               obj.velocity = cellfun(@(lr, grad) -lr*grad, lr, grad, ...
+                                      'UniformOutput', false);
+            end  
+            step = obj.velocity;
          else
             if isscalar(lr)
-               model.increment_params(cellfun(@(vel) rho*vel, obj.velocity, ...
-                                                 'UniformOutput', false));
-               grad = model.gradient(batch);
                obj.velocity = cellfun(@(grad, vel) rho*vel - lr*grad, grad, ...
-                                                obj.velocity, 'UniformOutput', false);
-               model.increment_params(cellfun(@(grad) -lr*grad, grad, 'UniformOutput', false));
+                                      obj.velocity, 'UniformOutput', false);
+               step = cellfun(@(grad) -lr*grad, grad, 'UniformOutput', false);
             else
-               model.increment_params(cellfun(@(rho, vel) rho*vel, rho, obj.velocity, ...
-                                                'UniformOutput', false));
-               grad = model.gradient(batch);
-               obj.velocity = cellfun(@(lr, rho, grad, vel) rho*vel - lr*grad, lr, rho, ...
-                                                grad, obj.velocity, 'UniformOutput', false);
-               model.increment_params(cellfun(@(lr, grad) -lr*grad, lr, grad, ...
-                                                                  'UniformOutput', false));
-            end
-         end
+               obj.velocity = cellfun(@(lr, rho, grad, vel) rho*vel - lr*grad, ...
+                                      lr, rho, grad, obj.velocity, ...
+                                      'UniformOutput', false);
+               step = cellfun(@(lr, grad) -lr*grad, lr, grad, ...
+                                      'UniformOutput', false);               
+            end            
+         end         
       end
       
       function reset(obj)
