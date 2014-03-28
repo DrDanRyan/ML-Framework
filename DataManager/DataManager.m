88ec51c63aa9b8612e8ce570effe4e86ec08d97e
DrDanRyan <danryanfl@yahoo.com>
2014-03-28 15:10:10 -0400

    update documentation

--- DataManager/DataManager.m
+++ DataManager/DataManager.m
@@ -1,18 +1,30 @@
 classdef DataManager < matlab.mixin.Copyable
+   % Handles serving of batches for training and validation data.
    
    properties
-      trainingData % cell array of data relevant for training; targets should be last entry if present
-      validationData % cell array of data relevant for validation; same shape as trainingData
-      batchSize   % Size of batch used for training
-      lossBatchSize % Size of batch used to compute training and validation losses
-      trainLossSampleSize % number of training examples to sample for training loss
-      trainLossSample % a sample of the training data used to estimate loss on training set
-      trainingSize % number of training examples total
-      validationSize % number of validation examples total
+      % Cell array of data relevant for training;
+      % targets should be last entry if present
+      trainingData
       
-      % Used to keep track of batches
-      batchIdx
-      lossIdx
+      % Cell array of data relevant for validation; same shape as trainingData
+      validationData 
+      batchSize   % Size of batch used for training
+      
+      % Size of batch used to compute training and validation losses
+      lossBatchSize
+      
+      % Number of training examples to sample for training loss
+      trainLossSampleSize 
+      
+      % Sample of the training data used to estimate loss on training set
+      trainLossSample    
+      trainingSize % Number of training examples total
+      validationSize % Number of validation examples total
+      
+      batchIdx % Index of the beginning of the next training batch
+      
+      % Index of the beginning of the next training or validation loss batch
+      lossIdx 
    end
    
    methods
@@ -41,7 +53,7 @@ classdef DataManager < matlab.mixin.Copyable
          obj.lossBatchSize = p.Results.lossBatchSize;
          obj.trainLossSampleSize = p.Results.trainLossSampleSize;
          
-         if ~isempty(p.Results.batchSize) % Using mini-batches => shuffle data before beginning
+         if ~isempty(p.Results.batchSize) % Using mini-batches 
             obj.shuffle_training_data();
          end
          
@@ -51,12 +63,15 @@ classdef DataManager < matlab.mixin.Copyable
       end
       
       function batch = next_batch(obj)
+         % Returns the next training batch (cell array containing inputs,
+         % targets and possibly other data).
+         
          if isempty(obj.batchSize) % full batch
             batch = obj.trainingData;
          else % mini-batch
             stopIdx = min(obj.trainingSize, obj.batchIdx + obj.batchSize - 1);
-            batch = cellfun(@(v) v(:,obj.batchIdx:stopIdx,:,:), obj.trainingData, ...
-                              'UniformOutput', false);
+            batch = cellfun(@(v) v(:,obj.batchIdx:stopIdx,:,:), ...
+                              obj.trainingData, 'UniformOutput', false);
             
             if stopIdx == obj.trainingSize
                obj.shuffle_training_data();
@@ -67,10 +82,14 @@ classdef DataManager < matlab.mixin.Copyable
       end
       
       function [batch, isContinue] = trainLoss_batch(obj)
+         % Returns the next batch for computing mean training loss as well as a
+         % flag indicating whether there are more batches to come. Will take
+         % sample from training data if trainLossSampleSize is not empty.
+         
          if ~isempty(obj.trainLossSampleSize) && isempty(obj.trainLossSample)
             permvec = randperm(obj.trainingSize, obj.trainLossSampleSize);
-            obj.trainLossSample = cellfun(@(v) v(:,permvec,:,:), obj.trainingData, ...
-                                             'UniformOutput', false);
+            obj.trainLossSample = cellfun(@(v) v(:,permvec,:,:), ...
+                                    obj.trainingData, 'UniformOutput', false);
          end
          
          if isempty(obj.lossBatchSize) % no batches, just give whole set
@@ -83,9 +102,10 @@ classdef DataManager < matlab.mixin.Copyable
             isContinue = false;
          else % using batches
             if isempty(obj.trainLossSample)
-               stopIdx = min(obj.trainingSize, obj.lossIdx + obj.lossBatchSize - 1);
-               batch = cellfun(@(v) v(:,obj.lossIdx:stopIdx,:,:), obj.trainingData, ...
-                                 'UniformOutput', false);
+               stopIdx = min(obj.trainingSize, ...
+                             obj.lossIdx + obj.lossBatchSize - 1);
+               batch = cellfun(@(v) v(:,obj.lossIdx:stopIdx,:,:), ...
+                                    obj.trainingData, 'UniformOutput', false);
                               
                if stopIdx == obj.trainingSize
                   isContinue = false;
@@ -95,9 +115,10 @@ classdef DataManager < matlab.mixin.Copyable
                   obj.lossIdx = stopIdx + 1;
                end               
             else
-               stopIdx = min(obj.trainLossSampleSize, obj.lossIdx + obj.lossBatchSize - 1);
-               batch = cellfun(@(v) v(:,obj.lossIdx:stopIdx,:,:), obj.trainLossSample, ...
-                                 'UniformOutput', false);
+               stopIdx = min(obj.trainLossSampleSize, ...
+                             obj.lossIdx + obj.lossBatchSize - 1);
+               batch = cellfun(@(v) v(:,obj.lossIdx:stopIdx,:,:), ...
+                                   obj.trainLossSample, 'UniformOutput', false);
                               
                if stopIdx == obj.trainLossSampleSize
                   isContinue = false;
@@ -112,13 +133,17 @@ classdef DataManager < matlab.mixin.Copyable
       end
       
       function [batch, isContinue] = validLoss_batch(obj)
+         % Returns next batch for computing mean validation loss and a boolean
+         % flag indicating whether there are more batches yet to come.
+         
          if isempty(obj.lossBatchSize)
             batch = obj.validationData;
             isContinue = false;
          else
-            stopIdx = min(obj.validationSize, obj.lossIdx + obj.lossBatchSize - 1);
-            batch = cellfun(@(v) v(:,obj.lossIdx:stopIdx,:,:), obj.validationData, ...
-                                 'UniformOutput', false);
+            stopIdx = min(obj.validationSize, ...
+                          obj.lossIdx + obj.lossBatchSize - 1);
+            batch = cellfun(@(v) v(:,obj.lossIdx:stopIdx,:,:), ...
+                                 obj.validationData, 'UniformOutput', false);
 
             if stopIdx == obj.validationSize
                isContinue = false;
@@ -131,6 +156,9 @@ classdef DataManager < matlab.mixin.Copyable
       end
       
       function shuffle_training_data(obj)
+         % Randomly shuffles training data and resets batchIdx to 1. Called 
+         % after each full cycle through trainingData (if using minibatches). 
+         
          permvec = randperm(obj.trainingSize);
          obj.trainingData = cellfun(@(v) v(:,permvec,:,:), obj.trainingData, ...
                                                       'UniformOutput', false);
@@ -138,7 +166,11 @@ classdef DataManager < matlab.mixin.Copyable
       end
       
       function reset(obj)
+         % Recomputes trainingSize and validationSize (in case data has
+         % changed). Also resets batchIdx, lossIdx and shuffles trainingData.
+         
          obj.trainingSize = size(obj.trainingData{1}, 2);
+         
          if ~isempty(obj.validationData)
             obj.validationSize = size(obj.validationData{1}, 2);
          else
