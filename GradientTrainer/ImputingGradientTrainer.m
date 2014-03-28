1621c0937eab2924c5cd99e06a4ff82787df1dc8
DrDanRyan <danryanfl@yahoo.com>
2014-03-28 14:53:52 -0400

    update documentation

--- GradientTrainer/ImputingGradientTrainer.m
+++ GradientTrainer/ImputingGradientTrainer.m
@@ -1,4 +1,7 @@
 classdef ImputingGradientTrainer < GradientTrainer
+   % A GradientTrainer that passes imputed missing data from model back to
+   % dataManager after each update. Use this class if your model refines
+   % estimates for missing data in each batch as it learns.
    
    methods
       function isContinue = update(obj)
