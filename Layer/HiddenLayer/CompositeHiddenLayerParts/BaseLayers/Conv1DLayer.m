568b1c48b88b762ff70397494b0b25a99b48a73a
Daniel Ryan <danryanfl@yahoo.com>
2014-03-29 15:37:47 -0400

    update documentation; remove deprecated files

--- Layer/HiddenLayer/CompositeHiddenLayerParts/BaseLayers/Conv1DLayer.m
+++ Layer/HiddenLayer/CompositeHiddenLayerParts/BaseLayers/Conv1DLayer.m
@@ -1,5 +1,9 @@
-classdef Conv1DLayer < ParamsFunctions & matlab.mixin.Copyable
+classdef Conv1DLayer < CompositeBaseLayer & ParamsFunctions & ...
+                       matlab.mixin.Copyable
    % A convolution layer for multiple channels of 1D signals.
+   %
+   % inputs should be 3D arrays with dimensions:
+   % nChannels x batchSize x signalLength (C x N x X in the program comments)
    
    properties
       % params = {W, b} with W ~ nF x 1 x C x fS and b ~ nF x 1
@@ -10,7 +14,8 @@ classdef Conv1DLayer < ParamsFunctions & matlab.mixin.Copyable
    end
    
    methods
-      function obj = Conv1DLayer(inputSize, nChannels, filterSize, nFilters, varargin)
+      function obj = Conv1DLayer(inputSize, nChannels, filterSize, nFilters, ...
+                                 varargin)
          obj = obj@ParamsFunctions(varargin{:});
          obj.inputSize = inputSize;
          obj.nChannels = nChannels;
@@ -47,7 +52,8 @@ classdef Conv1DLayer < ParamsFunctions & matlab.mixin.Copyable
          Wx = obj.gpuState.zeros(obj.nFilters, N, X - obj.filterSize + 1);
          
          for i = 1:(X - obj.filterSize + 1)
-            xSeg = permute(x(:,:,i:i+obj.filterSize-1), [4, 2, 1, 3]); % 1 X N x C x fS
+            xSeg = permute(x(:,:,i:i+obj.filterSize-1), [4, 2, 1, 3]); 
+            % xSeg ~ 1 X N x C x fS
             Wx(:,:,i) = sum(sum(bsxfun(@times, xSeg, obj.params{1}), 4), 3);
          end
       end
@@ -58,18 +64,24 @@ classdef Conv1DLayer < ParamsFunctions & matlab.mixin.Copyable
          grad{2} = mean(sum(dLdy, 3), 2);
          grad{1} = obj.gpuState.zeros(size(obj.params{1})); % nF x 1 x C x fS
          for i = 1:obj.filterSize
-            xSeg = permute(x(:,:,i:ySize + i - 1), [4, 2, 1, 3]); % 1 x N x C x Size
-            grad{1}(:,:,:,i) = mean(sum(bsxfun(@times, xSeg, permute(dLdy, [1, 2, 4, 3])), 4), 2); % nF x 1 x C
+            xSeg = permute(x(:,:,i:ySize + i - 1), [4, 2, 1, 3]); 
+            % xSeg ~ 1 x N x C x Size
+            grad{1}(:,:,:,i) = mean(sum(bsxfun(@times, xSeg, ...
+                              permute(dLdy, [1, 2, 4, 3])), 4), 2); % nF x 1 x C
          end
          
          [nF, N, ~] = size(dLdy);
-         dLdx = obj.gpuState.zeros(obj.inputSize, N, obj.nChannels); % X x N x C (need to permute before returning)
+         dLdx = obj.gpuState.zeros(obj.inputSize, N, obj.nChannels); 
+         % dLdx ~ X x N x C (need to permute before returning)
          dLdyPadded = cat(4, obj.gpuState.zeros(nF, N, 1, obj.filterSize-1), ...
                               permute(dLdy, [1, 2, 4, 3]), ...
-                              obj.gpuState.zeros(nF, N, 1, obj.filterSize-1)); % nF x N x 1 x (X + fS - 1)
+                              obj.gpuState.zeros(nF, N, 1, obj.filterSize-1)); 
+         % dLdyPadded ~ nF x N x 1 x (X + fS - 1)
+         
          for i = 1:obj.inputSize
             dLdySeg = dLdyPadded(:,:,:,i:i+obj.filterSize-1); % nF x N x 1 x fS
-            dLdx(i,:,:) = sum(sum(bsxfun(@times, dLdySeg, flip(obj.params{1}, 4)), 4), 1); % 1 x N x C
+            dLdx(i,:,:) = sum(sum(bsxfun(@times, dLdySeg, ...
+                                    flip(obj.params{1}, 4)), 4), 1); % 1 x N x C
          end
          dLdx = permute(dLdx, [3, 2, 1]);
       end      
