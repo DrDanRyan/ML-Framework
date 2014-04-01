ba150582f37fac2ad8eea03f0239892e831ace89
DrDanRyan <danryanfl@yahoo.com>
2014-04-01 11:17:06 -0400

    Rename GradientTrainer to Trainer

--- Trainer/ImputingTrainer.m
+++ Trainer/ImputingTrainer.m
@@ -1,5 +1,5 @@
-classdef ImputingGradientTrainer < GradientTrainer
-   % A GradientTrainer that passes imputed missing data from model back to
+classdef ImputingTrainer < Trainer
+   % A Trainer that passes imputed missing data from model back to
    % dataManager after each update. Use this class if your model refines
    % estimates for missing data in each batch as it learns.
    
